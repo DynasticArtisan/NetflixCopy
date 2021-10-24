284431b33ad14bf662e77a987484480b8cfdce0b
DynasticArtisan <panteleeev.aleksandr@gmail.com>
2021-10-24 13:48:02 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAvuWs8Z8Vuj5GAdxkA70ZHUF6R9TxM4P0",
+    authDomain: "netflix-clone-d2d8e.firebaseapp.com",
+    projectId: "netflix-clone-d2d8e",
+    storageBucket: "netflix-clone-d2d8e.appspot.com",
+    messagingSenderId: "544371857241",
+    appId: "1:544371857241:web:cad0058d32312350109e48"
+  };
